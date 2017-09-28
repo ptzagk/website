441a750201a3c6d00bf9d2f5b523d20403028eb4
Swizz <gerodel.quentin@gmail.com>
2017-09-28 14:59:12 +0200

    Remove empty props object

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { main, svg, h1, span, a as link, button } from "@hyperapp/html"
 
 import "./index.css"
 
-const icon = href => svg({}, use(href))
+const icon = href => svg([use(href)])
 const use = href =>
   h("use", {
     oncreate: e =>
@@ -19,13 +19,13 @@ app({
   actions: {},
   events: {},
   view: (s, a) =>
-    main({}, [
+    main([
       icon("hyperapp"),
-      h1({}, "1 KB JavaScript library for building frontend applications"),
+      h1("1 KB JavaScript library for building frontend applications"),
       h("row-", { class: "social", gap: "1" }, [
         link({ href: "https://github.com/hyperapp/hyperapp" }, [
           icon("github"),
-          span({}, "GITHUB")
+          span("GITHUB")
         ]),
         link({ href: "https://hyperappjs.herokuapp.com" }, [
           icon("slack"),
@@ -33,19 +33,19 @@ app({
         ]),
         link({ href: "https://twitter.com/hyperappjs" }, [
           icon("twitter"),
-          span({}, "TWITTER")
+          span("TWITTER")
         ])
       ]),
       h("row-", { class: "reading", gap: "1" }, [
         link({ href: "https://github.com/hyperapp/hyperapp/tree/master/src" }, [
-          span({}, "READ THE CODE")
+          span("READ THE CODE")
         ]),
         link(
           {
             href:
               "https://github.com/hyperapp/hyperapp/blob/master/docs/README.md#documentation"
           },
-          [span({}, "READ THE DOCS")]
+          [span("READ THE DOCS")]
         )
       ])
     ])
