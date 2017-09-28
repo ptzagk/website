acf857b0709d384d9ff3476bfd2bdf06f854dcb3
Swizz <gerodel.quentin@gmail.com>
2017-09-28 14:35:55 +0200

    Add official @hyperapp/html helpers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { h, app } from "hyperapp"
+import { main, svg, h1, span, a as link, button } from "@hyperapp/html"
+
 import "./index.css"
 
-const main = (p, c) => h("main", p, c)
-const svg = (p, c) => h("svg", p, c)
 const icon = href => svg({}, use(href))
-const h1 = (p, c) => h("h1", p, c)
-const span = (p, c) => h("span", p, c)
-const link = (p, c) => h("a", p, c)
-const button = (p, c) => h("button", p, c)
 const use = href =>
   h("use", {
     oncreate: e =>
